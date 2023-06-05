1909d990d451ab01689e8e794508c1219dba426d
Denis <Denis@super.com>
2023-06-06 00:16:30 +0400

    Lesson 4_2_9

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-﻿import pytest
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-﻿from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
